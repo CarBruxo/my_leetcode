c89a8d2595be81dc61b316218f4de6d1b6a83724
Carlos <101020432+CarBruxo@users.noreply.github.com>
2024-01-09 09:19:19 -0300

    Update twosum_v2.py
    added coments explaining v2 in pt-br

--- twosum_v2.py
+++ twosum_v2.py
@@ -13,13 +13,14 @@ for a in nums2:
     for b in nums2:
         c = a + b
         if c == target:
-            a_pos=nums.index(a)
-            b_pos=nums.index(b)
-            indexes=[a_pos, b_pos]
-            reversed_indexes=indexes[::-1]
-            ocurance.append(indexes)
+            a_pos=nums.index(a)                                 #posição na lista nums de a
+            b_pos=nums.index(b)                                 #posição na lista nums de b
+            indexes=[a_pos, b_pos]                              #as duas posições juntas em uma lista [a, b]
+            reversed_indexes=indexes[::-1]                      #posições revertidas usando técnica de slicing, na qual N posição se torna a primeira (0) tal que [b, a] se torne [a, b]
+            ocurance.append(indexes)                            #adiciona [a, b] na lista de possiveis opções
 
-            if ocurance.count(reversed_indexes) >= 1:
-                ocurance.remove(indexes)
+            if ocurance.count(reversed_indexes) >= 1:           #usando count([a,b]) para ver a quantidade de vezes que [a, b] ocorre na lista de possibilidades, 0 para se nao ocorre, 1 ou + caso já exista.
+                ocurance.remove(indexes)                        #caso já existir [a, b], [b, a] seré removido da lista. Já que [a, b] = [b, a] no contexto do código.
+                
 print(f"{nums} is the input list")
 print(f"{ocurance} are the sum options wich outputs target")
